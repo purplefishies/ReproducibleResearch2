0da320739389c595acaa19ec70ab97fd437481f5
Jimi Damon <jdamon@gmail.com>
2015-06-21 12:16:59 -0700

    Tmp, work moving along , able to filter

--- tmp.R
+++ tmp.R
@@ -5,6 +5,30 @@ library(lubridate)
 
 
 dat <- read.csv("stormdata.csv")
+ddat <- tbl_df(dat)
+
+
+
+#Custom filters
+
+tornadoes <- ddat %>%
+    filter( !is.na(F) ) %>%
+    group_by(F) %>%
+    mutate( EVTYPE=ifelse(!is.na("F"),paste("TORNADO_F",as.character(F),sep=""),"BLAH"))  %>%
+    group_by( EVTYPE ) %>% 
+    summarise( ave_fatal = mean(FATALITIES,rm.na=TRUE),
+               ave_injuries=mean(INJURIES,rm.na=TRUE),
+               ave_propdmg = mean(PROPDMG,rm.na=TRUE),
+               ave_cropdmg = mean(CROPDMG,rm.na=TRUE)
+              )
+
+hurricaines <-
+ddat %>%
+    filter( grepl(".*hail.*", EVTYPE,perl=T,ignore.case=T) )%>%
+    select(EVTYPE)
+
+
+
 
 
 
